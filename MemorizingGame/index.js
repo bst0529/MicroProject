1455bf047121b1fefe490997a9c3f50afee4dcd1
Liu Chun-Kai <bst0529@hotmail.com>
2022-08-12 16:38:13 +0800

    配對失敗加入 CSS 動畫

--- MemorizingGame/index.js
+++ MemorizingGame/index.js
@@ -56,6 +56,15 @@ const view = {
     },
     renderTriedTimes (times){
         document.querySelector('.tried').textContent = `You've tried: ${times} times`
+    },
+    appendWrongAnimation (...cards) {
+        console.log(111)        
+        cards.map(card => {
+            card.classList.add('wrong')
+            card.addEventListener('animationend', event => {
+                event.target.classList.remove('wrong')
+            },{ once: true })
+        })
     }
 }
 const utility = {
@@ -104,6 +113,7 @@ const controller = {
                 } else {
                     //配對失敗
                     this.currentState = GAME_STATE.CardsMatchFailed
+                    view.appendWrongAnimation(...model.revealedCards)
                     setTimeout(this.resetCards, 1000);
                 }
                 break
