5304b40c14139285c9d7fd4ab4c641615097f9d1
Liu Chun-Kai <bst0529@hotmail.com>
2022-12-06 17:23:39 +0800

    初建 RestaurantList 專案

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 微型專案集
 1. MemorizingGame：翻牌遊戲
+2. MovieList：電影清單
+3. Restaurant：餐廳清單
---
+++ RestaurantList/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const {engine} = require('express-handlebars')
+const port = 3000
+
+const app = express()
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.use(express.static('public'))
+
+const restaurantList = require('./restaurant.json')
+
+app.get('/',(req,res) => {
+    res.render('index',{restaurant: restaurantList})
+})
+app.get('/show',(req,res) => {
+    res.render('show')
+})
+
+app.listen(port,() => {
+    console.log(`The Web Is Running On http:localhost:${port}`)
+})
+
+
