e9260dc0f08fa82de88281fb2c61328cc52a2c2e
Liu Chun-Kai <bst0529@hotmail.com>
2022-12-08 15:03:40 +0800

    RestaurantList：index、show頁
    1. 讀取 JSON 檔案，將種子資料載入應用程式
    2. 把資料帶入 handlebars 樣板中動態呈現
    3. 操作 handlebars 中的 each 迴圈呈現出多張餐廳卡片
    4. 應用 params 打造動態路由
    5. 用 Query String 打造搜尋功能
    6. 搜尋沒有結果時也有對應頁面提示

--- RestaurantList/app.js
+++ RestaurantList/app.js
@@ -12,10 +12,19 @@ app.use(express.static(__dirname + '/public'))
 const restaurantList = require('./restaurant.json')
 
 app.get('/',(req,res) => {
-    res.render('index',{restaurant: restaurantList})
+    res.render('index',{restaurants: restaurantList.results})
 })
-app.get('/show',(req,res) => {
-    res.render('show')
+app.get('/restaurants/:id',(req,res) => {
+    const restaurant = restaurantList.results.find(r => r.id.toString() === req.params.id)
+    res.render('show',{restaurant: restaurant})
+})
+app.get('/Search',(req,res) => {
+    const keyword = req.query.keyword
+    const restaurants = restaurantList.results.filter(r => {
+        return r.category.toLowerCase().includes(keyword.toLocaleLowerCase())
+            || r.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
+    })
+    res.render('index',{restaurants: restaurants,keyword: keyword})
 })
 
 app.listen(port,() => {
